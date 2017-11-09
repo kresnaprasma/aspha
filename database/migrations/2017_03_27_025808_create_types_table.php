ca13118f49df27a3feedf4a710896756004e1d7e
Kresna Prasmadewa <kresnaprasmadewa@gmail.com>
2017-11-09 15:00:49 +0700

    Pull Request

--- database/migrations/2017_03_27_025808_create_types_table.php
+++ database/migrations/2017_03_27_025808_create_types_table.php
@@ -13,7 +13,7 @@ class CreateTypesTable extends Migration
     public function up()
     {
         Schema::create('types', function (Blueprint $table) {
-            $table->string('id');
+            $table->string('id')->index();
             $table->string('name');
             
             $table->string('merk_id');
